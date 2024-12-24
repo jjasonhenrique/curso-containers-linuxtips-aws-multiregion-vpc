b7ba947df25b5ae49814a20e119594ec8c48b466
Jason Henrique <jhmr.jason@outlook.com>
2024-12-24 08:00:47 -0300

    Update var

--- post-deployment/providers.tf
+++ post-deployment/providers.tf
@@ -1,14 +1,8 @@
 provider "aws" {
-  shared_config_files      = ["/home/jason/.aws/config"]
-  shared_credentials_files = ["/home/jason/.aws/credentials"]
-  profile                  = "blog"
   region                   = var.region
 }
 
 provider "aws" {
   alias                    = "peer"
-  shared_config_files      = ["/home/jason/.aws/config"]
-  shared_credentials_files = ["/home/jason/.aws/credentials"]
-  profile                  = "blog"
   region                   = var.region_peer
 }
